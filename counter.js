5d412b786fa36bab512dc5ff618d8b627fab89af
wprojectjkt <projectweekend9@gmail.com>
2025-08-26 15:45:56 +0700

    Delete counter.js

--- counter.js
+++
@@ -1,7 +0,0 @@
-const viewerCountEl = document.getElementById("viewerCount");
-const wsCounter = new WebSocket("wss://bot.wproject.web.id/counter");
-
-wsCounter.onmessage = (event) => {
-  const count = JSON.parse(event.data).viewers;
-  viewerCountEl.textContent = `👥 Penonton: ${count}`;
-};
