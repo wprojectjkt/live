ccfea23904716e17d5849c6e8b55432e0735ae3e
wprojectjkt <projectweekend9@gmail.com>
2025-09-06 04:31:13 +0700

    UIUI

--- js/player.js
+++ js/player.js
@@ -3,13 +3,52 @@ const HLS_URL = "https://stream.wproject.web.id/hls/teststream.m3u8";
 
 function startPlayer() {
   const video = document.getElementById("videoPlayer");
+
   if (Hls.isSupported()) {
     const hls = new Hls();
     hls.loadSource(HLS_URL);
     hls.attachMedia(video);
+
+    // Setelah manifest dimuat
+    hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+      buildQualitySelector(hls, data.levels);
+    });
   } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
     video.src = HLS_URL;
   }
+}
+
+function buildQualitySelector(hls, levels) {
+  const controlBar = document.querySelector(".custom-controls");
+
+  if (!levels || levels.length <= 1) return;
+
+  const select = document.createElement("select");
+  select.className = "quality-selector";
+
+  // Auto option
+  const autoOption = document.createElement("option");
+  autoOption.value = -1;
+  autoOption.text = "Auto";
+  select.appendChild(autoOption);
+
+  levels.forEach((lvl, i) => {
+    const opt = document.createElement("option");
+    opt.value = i;
+    opt.text = `${lvl.height}p`;
+    select.appendChild(opt);
+  });
+
+  select.addEventListener("change", () => {
+    const value = parseInt(select.value);
+    if (value === -1) {
+      hls.currentLevel = -1; // auto
+    } else {
+      hls.currentLevel = value;
+    }
+  });
+
+  controlBar.appendChild(select);
 }
 
 function initPlayer() {
